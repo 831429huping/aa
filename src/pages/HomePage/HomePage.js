16c80d800d62c22c03638ac04382c713a75cb2ab
ZhouXizi <chowxizi@gmail.com>
2020-10-04 09:26:52 -0700

    Styled Components

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./HomePageStyle.scss";
+import DirectoryMenu from "../../components/DirectoryMenu/DirectoryMenu";
+const HomePage = () => <DirectoryMenu />;
+
+export default HomePage;
