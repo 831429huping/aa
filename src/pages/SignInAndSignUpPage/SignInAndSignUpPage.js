b846eb20168fd204017242084f71b89033733e39
ZhouXizi <chowxizi@gmail.com>
2020-10-07 17:38:56 -0700

    Added Sign In Form

---
+++ src/pages/SignInAndSignUpPage/SignInAndSignUpPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SignIn from "../../components/SignIn/SignIn";
+
+import "./SignInAndSignUpStyle.scss";
+
+const SignInAndSignUpPage = () => (
+  <div>
+    <h1>Sign In</h1>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
